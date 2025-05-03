4d7a25b2f132f8398ea61334eca122689100c11b
LuisFuentesDev <lsfuentes2@gmail.com>
2025-05-03 15:19:26 -0400

    se fija color iconos de status bar

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
+import {StatusBar} from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaProvider>
+      <StatusBar barStyle="dark-content" />
       <AppNavigator />
     </SafeAreaProvider>
   );
